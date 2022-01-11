968afb92e691d455fb6a357b07f20f22bbfb9ae0
jackup <yk.58@hotmail.com>
2022-01-11 21:14:55 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+console.log('Merhaba')
+console.log(1234)
+console.log(true)
+console.error('hata oluştu')
+console.warn('bir uyarı oluştu')
+console.info('başarılı')
+//yorum satırı
+/*
+yorum
+satırları
+*/
---
+++ javascriptDegiskenler.js
@@ -0,0 +1,22 @@
+var age;
+
+age = 20;
+
+console.log(age);
+console.log(typeof age);
+var fullName = 'Yakup Kömük';
+
+console.log(fullName);
+console.log(typeof fullName);
+
+let isActive = false;
+console.log(isActive);
+console.log(typeof isActive);
+
+let job = null;
+console.log(job);
+console.log(typeof job);
+
+let names = ['ali', 'ahmet', 'can'];
+console.log(names);
+console.log(typeof names);
